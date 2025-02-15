50ac89ad529c1be64f8d64091e9927778bd0c568
natonyastewart <natonyastewart15@gmail.com>
2025-02-15 11:18:28 -0500

    Created my first Flask app

--- app.py
+++ app.py
@@ -6,9 +6,16 @@ app = Flask(__name__)
 # Routing for your application.
 # Put your routes below this comment
 '''
-
+@app.route('/')
+def home ():
+    return "My home page"
 
 @app.errorhandler(404)
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+if __name__ == "__main__":
+    app.run (debug=True)
+
+    
